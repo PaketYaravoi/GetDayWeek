6d89c2d81d9a5bcc8d1a1e329ba6ec6e0d65b20d
Roman <roma.klimov10.94@gmail.com>
2023-04-19 22:09:54 +1100

    Создал приложение для поиска выходных дней по числу)

--- tesk15_week/Program.cs
+++ tesk15_week/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Clear();
+Console.Write("Введите число дня недели: ");
+string input = Console.ReadLine()!;
+int number = int.Parse(input);
+Console.WriteLine(number);
+
+if(number > 7 || number < 1)
+{
+    Console.WriteLine("Недопустимое значение");
+    
+}
+else
+{
+    if(number == 6 || number == 7)
+    {
+        Console.WriteLine("Да!");
+    }
+    else
+    {
+        Console.WriteLine("Нет!");
+    }
+}
